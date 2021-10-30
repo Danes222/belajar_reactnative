ee5e970c443e59d7aebe85afbdc4dd877a64d376
Febri Ahmad Nurhidayat <92903488+febriahmadn@users.noreply.github.com>
2021-10-30 10:52:04 +0700

    Update feed.component.js
    update

--- screens/feed.component.js
+++ screens/feed.component.js
@@ -1,28 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { SafeAreaView } from 'react-native';
 import { Divider, Button, Layout, Text } from '@ui-kitten/components';
 
-const search = (sq='velocipastor') => {
-      fetch('http://www.omdbapi.com/?apikey=f4674f5&s='+sq)
-      .then(response => response.json())
-      .then(json => console.log(json))
-  }
+
 
 export const FeedScreen = ({ navigation }) => {
-  
-  var text = [];
-  for (let i = 0; i < 10; i++) {
-    text.push(
-      <Text key={i} category='h6'>This Is Feed.</Text>
-    )
-  }
+      const [data, setData] = useState(false);
+      
+      
+      const search = (sq='velocipastor') => {
+            fetch('http://www.omdbapi.com/?apikey=f4674f5&s='+sq)
+            .then(response => response.json())
+            .then(json => {
+                  setData(json.Search)
+            })
+      }
+      
+  //var text = [];
+  //for (let i = 0; i < 10; i++) {
+  //  text.push(
+  //    <Text key={i} category='h6'>This Is Feed.</Text>
+  // )
+  //}
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
       {/* <TopNavigation title='MyApp' alignment='center' accessoryLeft={BackAction}/> */}
       <Divider/>
       <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        { text }
+            {
+                  data.map((item, index) => {
+                        return(
+                              <View>
+                                    <Text>{item.Title}</Text>
+                                    <Text>{item.Year}</Text>
+                              </View>
+                        )
+                  })
+            }
         <Button style={{ marginVertical: 10 }} onPress={ search() } title='CARI FILM'>CARI FILM</Button>
       </Layout>
     </SafeAreaView>
